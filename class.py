de6d8d810ca05dc71d80df4a077d3048a2c33800
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:36:50 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,18 @@
+class hello:
+     def __init__(self,a="Hello ICT Academy"):
+         self.a=a
+
+     def display(self):
+         print(self.a)
+obj=hello()
+obj.display()
+
+
+class test:
+    def __init__(self,x):
+        self.x=x
+ 
+    def display(self):
+        print(self.x)
+obj=test()
+obj.display()
