d8eff3627a19d0956cb7db49d6127abbde203102
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:59:45 +0530

    Create unpacking sequences.py

---
+++ unpacking sequences.py
@@ -0,0 +1,14 @@
+course="python","amey",101
+student="Namaratha",34,course
+name,roll,regcourse=student #unpacking variables
+print (name)
+print(roll)
+print(regcourse)
+w,x,y,z="amey"#..helps in printing strings
+print(w,x,y,z)
+
+# (x,y) = (1,(2,4))[1:]
+# print(y)
+
+# (x,y) = (1,(2,4))[1]
+# print(y)
