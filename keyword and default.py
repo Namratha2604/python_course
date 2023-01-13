acc0f3ad4bf29fef01fa0d407e9caf9171beb7ca
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:48:39 +0530

    Create keyword and default.py

---
+++ keyword and default.py
@@ -0,0 +1,10 @@
+def printName(first, last, onlyLast=False):
+    if onlyLast:     
+        print (last) 
+    else:    
+        print (first, last)
+# printName('Anthony', 'Gonsalves', True)
+# printName('Anthony', 'Gonsalves')
+# printName('Gonsalves', first='Anthony')
+# printName(last='Anthony',first='Gonsalves', onlyLast='True')
+printName(last='Anthony',first='Gonsalves')
