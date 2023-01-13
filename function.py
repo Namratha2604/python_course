8538eec58d86dc9592b1483256bcd735069416c6
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:44:04 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,16 @@
+if (a>b):
+    return a
+else:
+    return b
+.....code to read max of a,b,c is..
+m=max(a,b)
+m=max(m,c)
+#functions allows to break up complex problem into small sub problems.
+
+def f(y):
+    ''' compute the
+        square of y '''
+    return (y*y)
+
+x = f(10/2)
+print (x)
