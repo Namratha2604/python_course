b9d1420fbee0cfcbc2e2879d8bd79af78f45321c
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 13:00:29 +0530

    Create while loop.py

---
+++ while loop.py
@@ -0,0 +1,29 @@
+n = int(input('Enter a number?'))
+i = 1
+while i < n:
+  print (i)
+  i = i+1
+
+n = 13 
+sum = 0 
+while (n > 1): 
+ n = n//2 
+ sum = sum + n 
+print(sum)
+
+n,p = 9,1
+while (n >= 1):   
+  n,p = n//2,n*p
+  print(int(p))
+
+n = int(input('Enter a number?'))
+i = n
+while i > 0 :
+  print (i)
+  i = i-1
+
+n = int(input('Enter a number?'))
+i = 1
+while i <= n:
+  i = i+1
+  print (i)
