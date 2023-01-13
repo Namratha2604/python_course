2faa694720861a6a8ce858367cfb74b994e38883
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:58:10 +0530

    Create special methods.py

---
+++ special methods.py
@@ -0,0 +1,13 @@
+#__init__--->called whenever a new object is created for a class
+#use default arguments to mimic multiple constructor
+#__str__--->called by str(object) and the builtin fuction format() and print()
+#to compute informal or nicely printable string representation of an obeject
+#COMPARISION
+#__lt__,__le__,__gt__,__ge__,__eq__,__nq__
+#<,<=,>,>=,==,!= are used with the object
+#AIRTHMETIC
+#__add__--->+
+#__sub__--->-
+#__mul__--->*
+#__truediv__--->/
+#__floordiv__--->//
