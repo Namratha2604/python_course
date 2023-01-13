4765f487884d3f4cd88ca0622c55e86f20ed813d
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:46:33 +0530

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,8 @@
+#first charector of the string has index=0
+name="Acads"
+print(name[3])
+#negative indeces start counting from the right
+#-1 means last,-2 means second last
+file="Robot"
+print(file[-1])
+#using an index that is too large or too small results in "index out of range" error
