663826fe9fa78f939ff6b5bf2c2cdc43c68c40e9
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:51:03 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,14 @@
+#printing multiplication table
+n=int(input("n=? "))
+i=1
+while (i<=10):
+    print(n,"X",i,"=",n*i)
+    i=i+1
+print("done")
+#printing multiplication table using while loop
+n=int(input("n=?"))
+i=1
+while (i<=10):
+    print(n,"X",i,"=",n*i)
+    i=i+1
+print("done")
