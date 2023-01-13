0eb03fb422ebd4c4eeb62873f01c89d02ca66cc5
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:57:08 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,11 @@
+name="Acads"
+print(name[0:3])
+#have to avoid the ending i.e 3 in this example
+print(name[:3])
+print(name[0:3]+name[3:])
+print(name[0:len(name)])
+# s = 'Hello'
+# print(s[len(s)])
+print(name[-4:-1])
+choo = "python"
+print(choo[ : :-1])#it will reverse the word
