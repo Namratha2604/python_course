ecbef5590673edbd2cb9a2c9faa2d466c15469bf
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-13 12:50:40 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,33 @@
+n=int(input("n=?"))
+x=1
+while (x<=n):
+    if(x%3==0) or (x%5==0):
+        print(x)
+x=x+1
+
+inp = 5
+while (inp > 0):      
+    last = input('next num? ')
+    inp = inp + 1
+print (last)
+
+n = 13
+while (n >= 2):   
+    if (n%2 ==0):
+        n = n//2
+    else:
+        n = 3*n + 1
+print(n)
+
+n = 13
+while (n > 1):   
+    n = n//2   
+print(n)
+
+N = int(input('Enter N:'))
+i = 1
+while ( i <= N ) :   
+    print(i)
+
+# len(1,2,3,4,5)
+    
